55e4182c06da72f7f326de2d3bb8245964e6bae9
RikitoIshizu <isizu69@gmail.com>
2024-05-26 17:52:16 +0900

    ディレクトリ整理

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,17 @@
 import BackButton from "@/components/Button/BackButton";
 import Header from "@/components/Header";
+import CommonFrame from "@/components/CommonFrame";
 
 export default function Register() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Header />
-      <section></section>
-      <BackButton />
-    </main>
+    <CommonFrame
+      title="プロフィール"
+      slotComponents={<div>プロフィールページだよ</div>}
+    />
+    // <main className="flex min-h-screen flex-col items-center justify-between">
+    //   <Header />
+    //   <section></section>
+    //   <BackButton />
+    // </main>
   );
 }
